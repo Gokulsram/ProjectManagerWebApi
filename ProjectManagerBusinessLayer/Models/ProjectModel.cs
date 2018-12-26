eea380f2195f86fb85d46a1afa5ee686a728d6ea
Gokulakrishnan <gokulsram@gmail.com>
2018-12-26 21:48:21 +0530

    Updated Priority

--- ProjectManagerBusinessLayer/Models/ProjectModel.cs
+++ ProjectManagerBusinessLayer/Models/ProjectModel.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace ProjectManagerBusinessLayer
 {
-   public class ProjectModel
+    public class ProjectModel
     {
         public int ProjectId { get; set; }
         public string ProjectName { get; set; }
         public DateTime? StartDate { get; set; }
         public DateTime? EndDate { get; set; }
-        public int Priority { get; set; }
-        public int UserId{ get; set; }
+        public int? Priority { get; set; }
+        public int UserId { get; set; }
         public int TasksCount { get; set; }
         public int CompletedTask { get; set; }
         public string UserName { get; set; }
