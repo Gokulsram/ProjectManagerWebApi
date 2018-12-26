04f8e794061cb3c4d1c5c55af446a7f9f72b6408
Gokulakrishnan <gokulsram@gmail.com>
2018-12-26 13:15:03 +0530

    Updated test cases

---
+++ ProjectManagerWebApi.Tests/HttpResponseTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace ProjectManagerWebApi.Tests
+{
+    [TestFixture]
+    public class HttpResponseTests
+    {
+        private HttpClient client;
+
+        private HttpResponseMessage response;
+
+        [SetUp]
+        public void SetUP()
+        {
+            client = new HttpClient();
+
+            client.BaseAddress = new Uri("http://localhost:52781/api/");
+            response = client.GetAsync("GetAllUsers").Result;
+        }
+
+        [Test]
+        public void GetResponseIsSuccess()
+        {
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+
+        [Test]
+        public void GetResponseIsJson()
+        {
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            Assert.AreEqual("application/json", response.Content.Headers.ContentType.MediaType);
+        }
+
+        [Test]
+        public void GetAuthenticationStatus()
+        {
+            Assert.AreNotEqual(HttpStatusCode.Unauthorized, response.StatusCode);
+
+        }
+    }
+}
---
+++ ProjectManagerWebApi.Tests/TaskControllerTest.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using ProjectManagerBusinessLayer;
+using ProjectManagerDataLayer;
+
+
+namespace ProjectManagerWebApi.Tests
+{
+    [TestFixture]
+    public class TaskControllerTest
+    {
+        UsersController contactsController;
+        IEnumerable<UsersModel> taskModels;
+        UsersRepository repository = new UsersRepository();
+
+        [SetUp]
+        public void Setup()
+        {
+            Mapper.Reset();
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<UsersModel, User>()
+                  .ForMember(vm => vm.User_ID, map => map.MapFrom(m => m.UserId))
+                .ForMember(vm => vm.FirstName, map => map.MapFrom(m => m.FirstName))
+                .ForMember(vm => vm.LastName, map => map.MapFrom(m => m.LastName))
+                .ForMember(vm => vm.Employee_ID, map => map.MapFrom(m => m.EmployeeId))
+                .ForMember(vm => vm.Task_ID, map => map.MapFrom(m => m.TaskId))
+                .ForMember(vm => vm.Project_ID, map => map.MapFrom(m => m.ProjectId));
+
+                cfg.CreateMap<User, UsersModel>()
+                    .ForMember(vm => vm.UserId, map => map.MapFrom(m => m.User_ID))
+                  .ForMember(vm => vm.FirstName, map => map.MapFrom(m => m.FirstName))
+                  .ForMember(vm => vm.LastName, map => map.MapFrom(m => m.LastName))
+                  .ForMember(vm => vm.EmployeeId, map => map.MapFrom(m => m.Employee_ID))
+                  .ForMember(vm => vm.TaskId, map => map.MapFrom(m => m.Task_ID))
+                  .ForMember(vm => vm.ProjectId, map => map.MapFrom(m => m.Project_ID));
+            });
+
+            UserBusiness business = new UserBusiness(repository);
+
+        }
+
+        [Test]
+        public void GetAllTaskTest()
+        {
+            // System.IO.File.AppendAllText(@"C:\Gokul_FSE\Git\logs.txt", "Step 1 ");
+            UserBusiness business = new UserBusiness(repository);
+            contactsController = new UsersController(business);
+            //Number of records
+            taskModels = contactsController.Get();
+            Assert.IsTrue(taskModels.Count() > 0);
+        }
+    }
+}
